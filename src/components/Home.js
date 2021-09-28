4ba61334306fe4d8654231816d3d88815f9ca7db
Justin <83990960+J-DeBruin@users.noreply.github.com>
2021-09-27 21:16:16 -0500

    day 1 commit

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from '../index';
+
+const Home = () => {
+    return (
+        <h1>Welcome Home!</h1>
+    )
+}
+
+export default Home;
---
+++ src/components/Login.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+import API from '../utilities/api';
+import TokenUtilities from '../utilities/token';
